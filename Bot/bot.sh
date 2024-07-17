912f960fe481b1aeceab84a2c1a835e2e861cb57
Garut-SisiBukit <aditiagilang345@gmail.com>
2024-07-17 17:57:00 +0700

    Update bot.sh

--- Bot/bot.sh
+++ Bot/bot.sh
@@ -36,7 +36,7 @@ apt install python3 python3-pip -y
 apt install sqlite3 -y
 cd /media/
 rm -rf cybervpn
-wget https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/cybervpn.zip
+wget https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/cybervpn.zip
 unzip cybervpn.zip
 cd cybervpn
 rm var.txt
@@ -110,86 +110,86 @@ systemctl enable cybervpn
 clear
 echo "downloading asset"
 
-wget -q -O /usr/bin/panelbot "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/panelbot.sh" && chmod +x /usr/bin/panelbot
+wget -q -O /usr/bin/panelbot "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/panelbot.sh" && chmod +x /usr/bin/panelbot
 
 
 
 
 
-wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/addnoobz.sh" && chmod +x /usr/bin/addnoobz
+wget -q -O /usr/bin/addnoobz "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/addnoobz.sh" && chmod +x /usr/bin/addnoobz
 
 
 
-wget -q -O /media/log-install.txt "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/log-install.txt"
+wget -q -O /media/log-install.txt "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/log-install.txt"
 
 
 
-wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/add-vless.sh" && chmod +x /usr/bin/add-vless
+wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/add-vless.sh" && chmod +x /usr/bin/add-vless
 
-wget -q -O /usr/bin/addtr "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/addtr.sh" && chmod +x /usr/bin/addtr
-
-
-wget -q -O /usr/bin/addws "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/addws.sh" && chmod +x /usr/bin/addws
+wget -q -O /usr/bin/addtr "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/addtr.sh" && chmod +x /usr/bin/addtr
 
 
-wget -q -O /usr/bin/addss "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/addss.sh" && chmod +x /usr/bin/addss
-
-wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
+wget -q -O /usr/bin/addws "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/addws.sh" && chmod +x /usr/bin/addws
 
 
+wget -q -O /usr/bin/addss "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/addss.sh" && chmod +x /usr/bin/addss
 
-wget -q -O /usr/bin/cek-ss "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/cek-ss.sh" && chmod +x /usr/bin/cek-ss
+wget -q -O /usr/bin/cek-ssh "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/cek-ssh.sh" && chmod +x /usr/bin/cek-ssh
 
 
 
-
-wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/cek-tr.sh" && chmod +x /usr/bin/cek-tr
-
-wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/cek-vless.sh" && chmod +x /usr/bin/cek-vless
-
-wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/cek-ws.sh" && chmod +x /usr/bin/cek-ws
-
-wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/del-vless.sh" && chmod +x /usr/bin/del-vless
+wget -q -O /usr/bin/cek-ss "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/cek-ss.sh" && chmod +x /usr/bin/cek-ss
 
 
 
 
-wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
+wget -q -O /usr/bin/cek-tr "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/cek-tr.sh" && chmod +x /usr/bin/cek-tr
+
+wget -q -O /usr/bin/cek-vless "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/cek-vless.sh" && chmod +x /usr/bin/cek-vless
+
+wget -q -O /usr/bin/cek-ws "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/cek-ws.sh" && chmod +x /usr/bin/cek-ws
+
+wget -q -O /usr/bin/del-vless "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/del-vless.sh" && chmod +x /usr/bin/del-vless
 
 
 
-wget -q -O /usr/bin/deltr "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/deltr.sh" && chmod +x /usr/bin/deltr
 
-wget -q -O /usr/bin/delws "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/delws.sh" && chmod +x /usr/bin/delws
-
-
-
-wget -q -O /usr/bin/delss "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/delss.sh" && chmod +x /usr/bin/delss
+wget -q -O /usr/bin/cek-noobz "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/cek-noobz.sh" && chmod +x /usr/bin/cek-noobz
 
 
 
-wget -q -O /usr/bin/renew-ss "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+wget -q -O /usr/bin/deltr "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/deltr.sh" && chmod +x /usr/bin/deltr
 
-
-wget -q -O /usr/bin/renewtr "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/renewtr.sh" && chmod +x /usr/bin/renewtr
-
-
-wget -q -O /usr/bin/renewvless "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/renewvless.sh" && chmod +x /usr/bin/renewvless
-
-
-wget -q -O /usr/bin/renewws "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/renewws.sh" && chmod +x /usr/bin/renewws
-
-
-wget -q -O /usr/bin/cek-mws "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/cek-mws.sh" && chmod +x /usr/bin/cek-mws
-
-wget -q -O /usr/bin/cek-mvs "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
-
-
-wget -q -O /usr/bin/cek-mss "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+wget -q -O /usr/bin/delws "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/delws.sh" && chmod +x /usr/bin/delws
 
 
 
-wget -q -O /usr/bin/cek-mts "https://raw.githubusercontent.com/Garut-SisiBukit/zexcek/main/Bot/cek-mts.sh" && chmod +x /usr/bin/cek-mts
+wget -q -O /usr/bin/delss "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/delss.sh" && chmod +x /usr/bin/delss
+
+
+
+wget -q -O /usr/bin/renew-ss "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/renew-ss.sh" && chmod +x /usr/bin/renew-ss
+
+
+wget -q -O /usr/bin/renewtr "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/renewtr.sh" && chmod +x /usr/bin/renewtr
+
+
+wget -q -O /usr/bin/renewvless "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/renewvless.sh" && chmod +x /usr/bin/renewvless
+
+
+wget -q -O /usr/bin/renewws "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/renewws.sh" && chmod +x /usr/bin/renewws
+
+
+wget -q -O /usr/bin/cek-mws "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/cek-mws.sh" && chmod +x /usr/bin/cek-mws
+
+wget -q -O /usr/bin/cek-mvs "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/cek-mvs.sh" && chmod +x /usr/bin/cek-mvs
+
+
+wget -q -O /usr/bin/cek-mss "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/cek-mss.sh" && chmod +x /usr/bin/cek-mss
+
+
+
+wget -q -O /usr/bin/cek-mts "https://raw.githubusercontent.com/Garut-SisiBukit/zexcex/main/Bot/cek-mts.sh" && chmod +x /usr/bin/cek-mts
 
 cp /tmp/var.txt /media/cybervpn
 rm -rf bot.sh
